2bfda7a079edecbd618baf305dfc94bf49eb6f3c
mizumint <eliuttth@gmail.com>
2025-01-24 16:22:35 +0100

    Added login rate limit

--- src/utils/rateLimiter.ts
+++ src/utils/rateLimiter.ts
@@ -13,3 +13,15 @@ export const registerLimiter = rateLimit({
     res.status(429).json({ message: "Too many registration attempts from this IP, please try again after 15 minutes" });
   },
 });
+
+export const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: "Too many login attempts from this IP, please try again after 15 minutes",
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req: Request, res: Response) => {
+    logger.warn("Login rate limit exceeded", { ip: req.ip });
+    res.status(429).json({ nessage: "Too many login attempts from this IP, please try agaun after 15 minutes" });
+  },
+});
