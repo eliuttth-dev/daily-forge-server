9fe4f482d152d60e8c71a43d38d77d12c2649eb8
mizumint <eliuttth@gmail.com>
2025-01-24 16:21:40 +0100

    Added Login handler

--- src/handlers/auth.handler.ts
+++ src/handlers/auth.handler.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { createNewUser } from "../models/user.model";
+import { createNewUser, loginUser } from "../models/user.model";
 import { sendErrorResponse, sendSuccessResponse } from "../utils/responseHandler";
 import { logger } from "../logger";
 
@@ -28,4 +28,36 @@ export const registerHandler = async (req: Request, res: Response): Promise<void
   }
 };
 
-export const loginHandler = () => {};
+export const loginHandler = async (req: Request, res: Response): Promise<void> => {
+  const { username, password } = req.body;
+
+  try {
+    logger.info("Attempting user login", { username });
+
+    const loginResult = await loginUser(username, password);
+
+    if (loginResult.isSuccess) {
+      logger.info("User logged in successfully");
+      sendSuccessResponse(res, {
+        message: "Login successfully",
+        data: {
+          id: loginResult.data!.id,
+          username: loginResult.data!.username,
+          email: loginResult.data!.email,
+          token: loginResult.data!.token,
+        },
+      });
+      return;
+    }
+
+    logger.warn("Login failed", { username, reason: loginResult.message });
+    sendErrorResponse(res, 401, loginResult.message);
+  } catch (err: unknown) {
+    logger.error("Unexpected error during user login", { error: err });
+    if (err instanceof Error) {
+      sendErrorResponse(res, 500, "Internal Server Error");
+    } else {
+      sendErrorResponse(res, 500, "An unexpected error occurred");
+    }
+  }
+};
