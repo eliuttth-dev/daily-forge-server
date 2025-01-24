08b0fda6badc42c8366aab7b427b7baed2a2c29e
mizumint <eliuttth@gmail.com>
2025-01-24 14:29:48 +0100

    Added logs

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,36 +1,56 @@
 import { RowDataPacket, ResultSetHeader, FieldPacket } from "mysql2/promise";
 import pool from "../config/dbConfig";
 import { UserData, UserCreationResponse } from "../interfaces";
+import { logger } from "../logger";
 
 export const createNewUser = async (data: UserData): Promise<UserCreationResponse> => {
   const { username, email, password } = data;
+  const connection = await pool.getConnection();
 
   try {
-    // check if user already exists
-    const selectQuery = "SELECT username, email FROM users WHERE username = ? AND email = ?";
-    const selectValues = [username, email];
-    const [selectRows] = await pool.query<RowDataPacket[]>(selectQuery, selectValues);
+    await connection.beginTransaction();
 
-    if (selectRows.length > 0)
-      return { isSuccess: false, status: "conflic", message: "Username or email already exists", data: { username, email } };
+    // check if user already exists
+    const selectQuery = "SELECT username, email FROM users WHERE username = ? OR email = ?";
+    const selectValues = [username, email];
+    const [selectRows] = await connection.query<RowDataPacket[]>(selectQuery, selectValues);
+
+    if (selectRows.length > 0) {
+      await connection.rollback();
+      logger.info("Username or email already exists");
+      return { isSuccess: false, status: "conflict", message: "Username or email already exists", data: { username, email } };
+    }
 
     // create new user
-    const query = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
-    const values = [username, email, password];
-    const [rows]: [ResultSetHeader, FieldPacket[]] = await pool.execute(query, values);
+    const insertQuery = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+    const insertValues = [username, email, password];
+    const [insertResults]: [ResultSetHeader, FieldPacket[]] = await connection.execute(insertQuery, insertValues);
 
-    console.log(rows.insertId);
+    await connection.commit();
 
-    return { isSuccess: true, status: "created", message: "User created successfully", data: { username, email } };
+    logger.info("User created successfully");
+
+    return {
+      isSuccess: true,
+      status: "created",
+      message: "User created successfully",
+      data: { id: insertResults.insertId, username, email },
+    };
   } catch (err: unknown) {
+    await connection.rollback();
+
     let errorMessage: string = "An unexpected error occured";
 
     if (err instanceof Error) errorMessage = err.message;
+
+    logger.error("Error creating new user");
 
     return {
       isSuccess: false,
       status: "error",
       message: errorMessage,
     };
+  } finally {
+    connection.release();
   }
 };
