b92d78d2292ac5435cf73d06b2aa8053fd23d20e
mizumint <eliuttth@gmail.com>
2025-01-24 14:18:29 +0100

    logs added

--- src/handlers/auth.handler.ts
+++ src/handlers/auth.handler.ts
@@ -1,19 +1,25 @@
 import { Request, Response } from "express";
 import { createNewUser } from "../models/user.model";
 import { sendErrorResponse, sendSuccessResponse } from "../utils/responseHandler";
+import { logger } from "../logger";
 
 export const registerHandler = async (req: Request, res: Response): Promise<void> => {
   const { username, email, password } = req.body;
 
   try {
+    logger.info("Attempting to register new user");
+
     const userData = await createNewUser({ username, email, password });
 
     if (userData.isSuccess) {
+      logger.info("User registered successfully");
       sendSuccessResponse(res, { message: "User registered successfully", data: userData });
       return;
     }
+    logger.warn("User registration failed", { reason: userData.message });
     sendErrorResponse(res, 409, userData.message);
   } catch (err: unknown) {
+    logger.error("Unexpected error during user registration", { error: err });
     if (err instanceof Error) {
       sendErrorResponse(res, 500, "Internal Server Error");
     } else {
