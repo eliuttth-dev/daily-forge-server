7e6eded4c6e084d165395e9a91b1d8b51b34dc22
mizumint <eliuttth@gmail.com>
2025-01-24 01:55:22 +0100

    Interfaces added

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -11,3 +11,14 @@ export interface UserCreationResponse {
   data?: UserData;
 }
 
+export interface ErrorResponse {
+  status: "error";
+  error: { message: string };
+  data: null;
+}
+
+export interface SuccessResponse {
+  status: "success";
+  data: unknown;
+  error: null;
+}
