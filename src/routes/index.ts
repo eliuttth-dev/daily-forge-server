a31eebb255b36e2c92014ac426e7bed9bb5c4152
mizumint <eliuttth@gmail.com>
2025-01-24 16:22:17 +0100

    Updated routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { registerLimiter } from "../utils/rateLimiter";
-import { registerHandler } from "../handlers/auth.handler";
-import { registerMiddleware, validateRegistration } from "../middlewares/auth.middleware";
+import { registerLimiter, loginLimiter } from "../utils/rateLimiter";
+import { registerHandler, loginHandler } from "../handlers/auth.handler";
+import { registerMiddleware, validateRegistration, loginMiddleware, validateLogin } from "../middlewares/auth.middleware";
 
 const router = express.Router();
 
 // Auth Routes
-router.post("/api/v1/register", validateRegistration, registerLimiter, registerMiddleware, registerHandler);
+router.post("/api/auth/register", validateRegistration, registerLimiter, registerMiddleware, registerHandler);
+router.post("/api/auth/login", validateLogin, loginLimiter, loginMiddleware, loginHandler);
 
 export default router;
